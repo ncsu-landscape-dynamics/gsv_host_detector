51c3aa70bdc3887c1cd4e56276b5e0b7d9876708
Brennen Farrell <btfarre2@ncsu.edu>
2024-10-02 09:40:44 -0400

    Implementing genus species key

---
+++ tree_inventory/code/GCExtractor.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from EcoNameTranslator import to_scientific, to_common, to_species
+
+ 
+
+cityDF = pd.read_csv("cleaned_cityDF.csv")
+print(cityDF.shape[0])
+numRows = cityDF.shape[0]
+i = 0
+prevFindings = {}
+def cleanScientific(scientificName):
+    global i
+    global numRows
+    i += 1
+    if i % 300 == 0 or i == 1:
+        print(str(i) + "/" + str(numRows))
+    if scientificName in prevFindings:
+        return prevFindings[scientificName]
+    try:
+        index = to_species([scientificName])
+        values = index[scientificName][0].split()
+        if len(values) > 2:
+            print(values)
+        prevFindings[scientificName] = [values[-2], values[-1]]
+        return values[-2], values[-1]
+    except:
+        return ["NA", "NA"]
+        
+
+
+cityDF = pd.read_csv("cleaned_cityDF.csv")
+cityDF[['genus_name', 'species_name']] = cityDF.apply(
+    lambda row: pd.Series(cleanScientific(row['unique_sciname'])),
+    axis=1,
+    result_type='expand'
+)
+cityDF.to_csv('genusSpeciesSheet.csv', index=False)
+
+# species_name, genus_name
+# generated using econameparser
